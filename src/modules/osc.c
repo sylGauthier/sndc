f606216dc331717f626ed6d5bfd4cb78ea4c5e08
Sylvain Gauthier <syg@pedantic.software>
2021-05-20 18:18:02 +1000

    sane amplitude default, expose amp/period offset

--- src/modules/osc.c
+++ src/modules/osc.c
@@ -10,6 +10,11 @@
 #define RES 2048
 #define OUT 0
 
+#define DEF_FRQ 0
+#define DEF_AMP 0.5
+#define DEF_PAR 0.5
+#define DEF_SPL 44100
+
 static int osc_process(struct Node* n);
 static int osc_setup(struct Node* n);
 
@@ -18,8 +23,9 @@ const struct Module osc = {
     {
         {"function",    DATA_STRING,                REQUIRED},
         {"freq",        DATA_FLOAT | DATA_BUFFER,   REQUIRED},
-        {"offset",      DATA_FLOAT | DATA_BUFFER,   OPTIONAL},
         {"amplitude",   DATA_FLOAT | DATA_BUFFER,   OPTIONAL},
+        {"p_offset",    DATA_FLOAT,                 OPTIONAL},
+        {"a_offset",    DATA_FLOAT,                 OPTIONAL},
         {"param",       DATA_FLOAT,                 OPTIONAL},
         {"duration",    DATA_FLOAT,                 REQUIRED},
         {"sampling",    DATA_FLOAT,                 OPTIONAL},
@@ -48,14 +54,15 @@ const char* interpNames[] = {
 };
 
 enum OscInputType {
-    FUN,
-    FRQ,
-    OFF,
-    AMP,
-    PRM,
-    DUR,
-    SPL,
-    ITP,
+    FUN, /* wave function */
+    FRQ, /* frequency */
+    AMP, /* amplitude */
+    POF, /* period offset */
+    AOF, /* amplitude offset */
+    PRM, /* wave parameter */
+    DUR, /* signal duration */
+    SPL, /* sampling rate */
+    ITP, /* interpolation to use on output */
     NUM_INPUTS
 };
 
@@ -141,7 +148,7 @@ static void make_square(float* buf, unsigned int size, float param) {
 
 static int osc_process(struct Node* n) {
     struct Data* out = n->outputs[OUT];
-    float f, s, d, t, amp, off, param, *data, wave[RES];
+    float f, s, d, t, amp, aoff, param, *data, wave[RES];
     const char* fun;
     unsigned int i, size;
     struct Buffer bwave;
@@ -155,14 +162,14 @@ static int osc_process(struct Node* n) {
     if (n->inputs[SPL]) {
         s = n->inputs[SPL]->content.f;
     } else {
-        s = 44100;
+        s = DEF_SPL;
     }
     if (n->inputs[PRM]) {
         param = n->inputs[PRM]->content.f;
         if (param < 0) param = 0;
         if (param > 1) param = 1;
     } else {
-        param = 0.5;
+        param = DEF_PAR;
     }
     size = d * s;
     if (!(data = malloc(size * sizeof(float)))) {
@@ -178,15 +185,15 @@ static int osc_process(struct Node* n) {
         return 0;
     }
 
-    t = 0;
+    t = data_float(n->inputs[POF], 0, 0);
+    aoff = data_float(n->inputs[AOF], 0, 0);
     for (i = 0; i < size; i++) {
         float x = (float) i / (float) size;
 
-        f = data_float(n->inputs[FRQ], x, 0);
-        amp = data_float(n->inputs[AMP], x, 1);
-        off = data_float(n->inputs[OFF], x, 0);
+        f = data_float(n->inputs[FRQ], x, DEF_FRQ);
+        amp = data_float(n->inputs[AMP], x, DEF_AMP);
 
-        data[i] = amp * interp(&bwave, t) + off;
+        data[i] = amp * interp(&bwave, t) + aoff;
 
         t += f / s;
         if (t > 1) t -= 1;
