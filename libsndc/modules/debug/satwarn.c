e4eb905189624b4886f8802f81b4b9fe195add07
Sylvain Gauthier <syg@pedantic.software>
2022-04-06 12:56:35 +1000

    add 'satwarn' module
    Simple module that checks its input for saturation (|x| > 1) and pass it
    through to its output for convenience. It prints a message in stderr
    with the number of saturating samples if there are any.

---
+++ libsndc/modules/debug/satwarn.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+#include <sndc.h>
+#include <modules/utils.h>
+
+static int satwarn_process(struct Node* n);
+
+/* DECLARE_MODULE(satwarn) */
+const struct Module satwarn = {
+    "satwarn", "Outputs warning in stderr if input buffer is saturating",
+    {
+        {"in",  DATA_BUFFER,    REQUIRED,   "input buffer to check"}
+    },
+    {
+        {"out", DATA_BUFFER,    REQUIRED,   "passthrough of input"}
+    },
+    NULL,
+    satwarn_process,
+    NULL
+};
+
+enum SatwarnInputType {
+    INP,
+
+    NUM_INPUTS
+};
+
+static int satwarn_process(struct Node* n) {
+    unsigned int i, size, numSat = 0;
+    float* data;
+
+    GENERIC_CHECK_INPUTS(n, satwarn);
+
+    n->outputs[0] = n->inputs[0];
+    size = n->inputs[0]->content.buf.size;
+    data = n->inputs[0]->content.buf.data;
+
+    for (i = 0; i < size; i++) {
+        if (data[i] > 1. || data[i] < -1.) {
+            numSat++;
+        }
+    }
+
+    if (numSat) {
+        fprintf(stderr, "%s: SATURATION WARNING: %d samples are saturated\n",
+                        n->name, numSat);
+    }
+    return 1;
+}
